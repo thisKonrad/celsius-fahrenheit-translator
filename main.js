f8fc64cb709d3a80e7d1050189879f7c73389eeb
Konrad von Wittich <127662701+thisKonrad@users.noreply.github.com>
2023-09-25 12:03:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 /* :: temperature translator :: main SCRIPT JS :: */
 
-/** here it returns 2 decimal places with the toFixed(value) Method -
+
+/** here it returns 2 decimal places 
+ *  with toFixed(value) Method -
  *  return whole numbers with Math.ceil()./floor instead -
  *  */ 
+
+
 
 /* :: fahrenheit to celsius :: */
 const tempFH = document.querySelector('#input-FH');
@@ -11,17 +15,29 @@ const btnCS = document.querySelector('#button-CS');
 
 /* enter keyCode = 13 */
 tempFH.addEventListener('keypress', (event)=>{
-        const fahrenheit = tempFH.value;
-        if( event.keyCode === 13 ){
-            let getCelsius = (( fahrenheit - 32 ) * 5 / 9).toFixed(2);
-            resultCelsius.innerText = ' ' + getCelsius +' '+'°'+'C';}
+
+    const fahrenheit = tempFH.value;
+
+    if( event.keyCode === 13 ){
+
+        let getCelsius = (( fahrenheit - 32 ) * 5 / 9).toFixed(2);
+
+        resultCelsius.innerText = ' ' + getCelsius +' '+'°'+'C';
+    }
 });
+
+
 /* the button */
 btnCS.addEventListener('click', ()=>{
-        const fahrenheit = tempFH.value;
-        let getCelsius = (( fahrenheit - 32 ) * 5 / 9).toFixed(2);
-        resultCelsius.innerText = ' ' + getCelsius +' '+'°'+'C';}
-);
+
+    const fahrenheit = tempFH.value;
+
+    let getCelsius = (( fahrenheit - 32 ) * 5 / 9).toFixed(2);
+
+    resultCelsius.innerText = ' ' + getCelsius +' '+'°'+'C';
+
+});
+
 
 
 /* :: celsius to fahrenheit :: */
@@ -30,14 +46,23 @@ const resultFahrenheit = document.querySelector('#result-FH');
 const btnFH = document.querySelector('#button-FH');
 
 tempCS.addEventListener('keypress', (event)=>{
+
     const celsius = tempCS.value;
+
     if( event.keyCode === 13 ){
+
         let getFahrenheit = (( celsius * 1.8 ) + 32).toFixed(2);
-        resultFahrenheit.innerText = ' ' + getFahrenheit +' '+'°'+'F';}
+
+        resultFahrenheit.innerText = ' ' + getFahrenheit +' '+'°'+'F';
+    }
 });
 
 btnFH.addEventListener('click', ()=>{
+
     const celsius = tempCS.value;
+
     let getFahrenheit = (( celsius * 1.8 ) + 32).toFixed(2);
-        resultFahrenheit.innerText = ' ' + getFahrenheit +' '+'°'+'F';}
+
+        resultFahrenheit.innerText = ' ' + getFahrenheit +' '+'°'+'F';
+    }
 );
